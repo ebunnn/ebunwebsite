8323e961fbfaaa444a9ef926baa16753561dc902
Ebun O <remioludemi@gmail.com>
2022-11-15 01:11:58 -0500

    added prof image and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './Navbar'
+import profImage from "./images/Professional_Pic.jpg"
+import Footer from "./Footer"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">   
         <Navbar />
+        <img className="app--image" alt="" src={profImage}/>
+        <Footer />
       </header>
     </div>
   );
