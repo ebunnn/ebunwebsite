f6e5516ee185e6dce17de64e0d3d827f6662b982
Ebun O <remioludemi@gmail.com>
2023-07-22 03:30:04 -0400

    pre-deploy commit

--- src/Body.js
+++ src/Body.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./Body.css"
 import { Helmet } from 'react-helmet';
-// import { ElfsightWidget } from 'react-elfsight-widget';
 import profImage from "./images/Headshot.jpg"
 
 
 export default function Body() {
-
-    // function Component() {
-    //     return <ElfsightWidget widgetId="225e036c-006b-4e86-bf3f-46271e0253dd" />;
-    //   }
     return (
         <div className="body--container">
             <div className="body--elements">
@@ -21,20 +16,12 @@ export default function Body() {
                 </div>
                 <img className="headshot--image" alt="" src={profImage}/>
             </div>
-            <Helmet>
-                <script src="https://static.elfsight.com/platform/platform.js" defer />
-            </Helmet>
-            <div className="elfsight-app-225e036c-006b-4e86-bf3f-46271e0253dd"></div>
-            {/* <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
-            <div class="elfsight-app-225e036c-006b-4e86-bf3f-46271e0253dd"></div>
-            <Helmet>
-                <script src="https://your-elfsight-widget-url.com/225e036c-006b-4e86-bf3f-46271e0253dd.js" defer />
-            </Helmet> */}
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+            <div className="body--contact">
+                <Helmet>
+                    <script src="https://static.elfsight.com/platform/platform.js" defer />
+                </Helmet>
+                <div className="elfsight-app-225e036c-006b-4e86-bf3f-46271e0253dd"></div>
+            </div>
         
         </div>
     )
