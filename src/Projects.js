7719f7a05016e4469977c1719ad79907d1cbc903
Ebun O <remioludemi@gmail.com>
2023-10-08 23:03:23 -0400

    added <p> to projects

--- src/Projects.js
+++ src/Projects.js
@@ -9,11 +9,18 @@ import ebunWeb from "./images/ebunWebImg.png"
 
 export default function Projects() {
 
-  const [newsImg, setnewsImg] = React.useState(newsSearch)
+  const [newsImg, setnewsImg] = React.useState(true)
+
+  let showNewsImg = newsImg ? newsSearch : newsSearch2
   
   function handleNewsImg() {
-    setnewsImg(newsSearch2)
+    setnewsImg(prevImg => {
+      return !prevImg
+    }
+      )
   }
+
+
   const sogremDescription = "Sogrem Associates Website represents one of my earliest achievements in\
   web development. Created using ReactJS, this professional platform served the tax service \
   company, \"Sogrem Associates\" providing them with an online presence."
@@ -26,7 +33,7 @@ export default function Projects() {
 
   const newsAPIDescription = "The News Search website employs the newsapi.com API to enable effortless searches in\
   three categories: Entertainment, Sports, and Technology. Users can input their search query, choose a category,\
-  and retrieve the latest articles with titles, authors, images, and descriptions. A click on the image to the left\
+  and retrieve the latest articles with titles, authors, images, and descriptions. A click on the image\
   provides a sample of the search results."
 
   const ebunWebDescription = "My portfolio website, meticulously crafted with ReactJS, serves as an exceptional showcase\
@@ -35,11 +42,12 @@ export default function Projects() {
   return (
     <div>
       <h1 className="projects--title">My Projects!</h1>
+      <p style={{color: "black"}}>Here are *some* of my projects!</p>
       <ProjectCard title="Sogrem Associates Website" image={sogrem} description={sogremDescription} projectLink="https://sogrem.com"/>
 
       <ProjectCard title="PrepAI Website" image={prepAI} description={prepAIDescription} projectLink="https://github.com/rdl4199/BuffPrepAI"/>
 
-      <ProjectCard title="API News Search" image={newsImg} description={newsAPIDescription} projectLink="https://github.com/ebunnn/news-search-api" clickMe={handleNewsImg}/>
+      <ProjectCard title="API News Search" image={showNewsImg} description={newsAPIDescription} projectLink="https://github.com/ebunnn/news-search-api" clickMe={handleNewsImg}/>
 
       <ProjectCard title="Ebun Oludemi Portfolio Website" image={ebunWeb} description={ebunWebDescription} projectLink="https://ebunnn.github.io/ebunwebsite/"/>
       
